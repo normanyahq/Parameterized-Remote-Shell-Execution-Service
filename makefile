49a76e7017cd033c7e014c35f15a52e67648884a
Heqing Ya <heqing.ya@graphsql.com>
2017-08-07 19:53:20 -0700

    Add src

---
+++ makefile
@@ -0,0 +1,14 @@
+PORT := 41414
+HOST := 0.0.0.0
+WORKER := 4
+
+.PHONY: install run
+
+install:
+	pip install -r requirements.txt
+
+run:
+	gunicorn -w $(WORKER) server:app -b $(HOST):$(PORT)
+
+debug:
+	export FLASK_APP=server.py && flask run -p $(PORT)
---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from subprocess import Popen, PIPE
+import json
+
+app = Flask(__name__)
+
+HelpMessage = """
+Usage:
+    POST command to this URL with following payload:
+        {"file": "...", args:[...]}
+    We are using this format to keep it the same with NodeJs spawnSync
+Example:
+    {"file": "ls", args: ["-l", "-a"]}
+Test with curl:
+    curl -X POST -H "Content-type: application/json" --data '{"file": "ls", "args":["-a", "-l"]}' localhost:41414
+"""
+
+@app.route("/", methods=["POST", "GET"])
+def commandExecutor():
+    if request.method == "GET":
+        return HelpMessage
+    elif request.method == "POST":
+        commandObject = request.get_json()
+        process = Popen([commandObject["file"]] + commandObject["args"],
+                        stdin=PIPE,
+                        stdout=PIPE,
+                        stderr=PIPE)
+        (stdout, stderr) = process.communicate(input=commandObject.get("input", ""))
+        return json.dumps({ "stdout": stdout,
+                            "stderr": stderr,
+                            "exit_code": process.returncode,
+                            "error": process.returncode!=0})
